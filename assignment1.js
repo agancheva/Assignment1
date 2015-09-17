aa3db98c6092f024a3754633640bb26a49fbbda5
agancheva <ana.gancheva@gmail.com>
2015-09-17 16:25:14 -0700

    Create assignment1.js

---
+++ assignment1.js
@@ -0,0 +1,153 @@
+//Step 1
+
+//var someMonth;
+
+//function thisMonth() {
+//var test = new Date();
+//var result = test.getMonth();
+//}
+
+//var MY_CONSTANT;
+//var summerMonths = ["June", "July", "August"];
+
+//function myLibrary() {
+//}
+
+//Step 2
+//3.14
+//"Hello!"
+//true
+//null
+
+//Step 3
+//var firstName = "Ana";
+//var lastName = "Gancheva";
+//var fullName = "(" + firstName + " " + lastName + ")";
+//window.alert(fullName);
+
+//var result = 2 * (10 / 5) + 16;
+//window.alert(result);
+
+//Step 4
+//var strFirstName;
+//var strLastName;
+//var strAddress;
+//var strCity;
+//var strState;
+//var numZipCode;
+//var numYourAge;
+//var strReferralSource;
+//var strMayWeContactYou;
+
+//Step 5
+//var strFirstName;
+//var strLastName;
+//strFirstName = "Ana";
+//strLastName = "Gancheva";
+//
+//var strAddress = "5360 Nobel Drive";
+//var strCity = "San Diego";
+//
+//var strState = "California", numZipCode = "92101";
+
+//Step 6
+//var first=16 + "years";
+//console.log(first);
+
+//var second=true + "blue";
+//console.log(second);
+
+//var third=12 + true;
+//console.log(third);
+
+//Step 7
+//var firstName = "Ana";
+//console.log("My name is " + firstName + " " + lastName + ".");
+//var lastName="Gancheva";
+
+//Step 8
+//Even when using the "\" to denote quations marks I couldn't get it to work.
+
+//Step 9
+//var x=0;
+//console.log(x*y);
+
+//Step 10
+//var typeOne=typeof "John";
+//console.log(typeOne);
+//var typeTwo=typeof 16;
+//console.log(typeTwo);
+//var typeThree=typeof false;
+//console.log(typeThree);
+//var typeFour=typeof [5,6,7];
+//console.log(typeFour);
+//var typeFive=typeof beta;
+//console.log(typeFive);
+
+//Step 11
+//window.alert("Hello " + Ana + ", welcome to the JavaScript class!");
+
+//Step 12
+//var name="Ana";
+//window.alert("Hello " + name + ", welcome to the JavaScript class!");
+
+//Step 13
+//var name="Ana";
+//var course="JavaScript";
+//window.alert("Hello " + name + ", welcome to the " + course + " class!");
+
+//Step 14
+//var name="Ana";
+//var course="JavaScript";
+//window.alert("Hello " + name + ". \nWelcome to the " + course + " class!");
+
+//Step 15
+//var name = window.prompt("Please enter your name");
+//var course="JavaScript";
+//window.alert("Hello " + name + ". \nWelcome to the " + course + " class!");
+
+//Step 16
+//var name = window.prompt("Please enter your name");
+//var course= window.prompt("What class are you taking?");
+//window.alert("Hello " + name + ". \nWelcome to the " + course + " class!");
+
+//Step 17
+//var x=10;
+//var y=20;
+//console.log(x+y);
+
+//Step 18
+//var x=20;
+//console.log(x+=20);
+
+//Step 19
+//var x=20;
+//console.log(x*=5);
+
+//Step 20
+//var x=2;
+//console.log(x/=1);
+
+//Step 21
+//var x = 3;
+//var y = 20;
+//console.log(x <= 5 && y >= 10);
+
+//Step 22
+//var x = 6;
+//var y = 9;
+//console.log(x > y);
+
+//Step 23
+//var widget=new Object;
+//console.log (widget);
+
+//Step 24
+//function widget() { }
+//var widget = new Object;
+//console.log(widget instanceof Object);
+
+//Step 25
+//function widget() { }
+//var widget = new Object;
+//console.log(widget instanceof String);
