7db8b2aefb6ab360d445571c97bec25616a52db7
agancheva <ana.gancheva@gmail.com>
2015-10-14 20:45:36 -0700

    Create assignment1.js

---
+++ assignment1.js
@@ -0,0 +1,101 @@
+//Step 1
+
+//var someMonth;
+
+//function thisMonth() {
+//var test = new Date();
+//var result = test.getMonth();
+//}
+
+//var MY_CONSTANT;
+//var summerMonths = ["June", "July", "August"];
+
+//function myLibrary() {
+//}
+
+//Step 2
+//3.14
+//"Hello!"
+//true
+//null
+
+//Step 3
+//var firstName = "Ana";
+//var lastName = "Gancheva";
+//var fullName = "(" + firstName + " " + lastName + ")";
+//window.alert(fullName);
+
+//var result = 2 * (10 / 5) + 16;
+//window.alert(result);
+
+//Step 4
+//var strFirstName;
+//var strLastName;
+//var strAddress;
+//var strCity;
+//var strState;
+//var numZipCode;
+//var numYourAge;
+//var strReferralSource;
+//var strMayWeContactYou;
+
+//Step 5
+//var strFirstName;
+//var strLastName;
+//strFirstName = "Ana";
+//strLastName = "Gancheva";
+//
+//var strAddress = "5360 Nobel Drive";
+//var strCity = "San Diego";
+//
+//var strState = "California", numZipCode = "92101";
+
+//Step 6
+//var first=16 + "years";
+//console.log(first);
+
+//var second=true + "blue";
+//console.log(second);
+
+//var third=12 + true;
+//console.log(third);
+
+//Step 7
+//var firstName = "Ana";
+//console.log("My name is " + firstName + " " + lastName + ".");
+//var lastName="Gancheva";
+
+//Step 8
+//var someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I am not sure about the former."';
+//console.log(someString);
+
+//Step 9
+//var x=0;
+//console.log(x*y);
+
+//Step 10
+//var typeOne=typeof "John";
+//console.log(typeOne);
+//var typeTwo=typeof 16;
+//console.log(typeTwo);
+//var typeThree=typeof false;
+//console.log(typeThree);
+//var typeFour=typeof [5,6,7];
+//console.log(typeFour);
+//var typeFive=typeof beta;
+//console.log(typeFive);
+
+//Step 11
+//window.alert("Hello " + Ana + ", welcome to the JavaScript class!");
+
+//Step 12
+//var name="Ana";
+//window.alert("Hello " + name + ", welcome to the JavaScript class!");
+
+//Step 13
+//var course="JavaScript";
+//window.alert(course);
+
+//Step 14
+var string="Hello Zak Ruvalcaba.\nWelcome to the JavaScript class!"
+window.alert(string);
